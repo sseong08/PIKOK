2267cb728a8c45c1635ecf35fc83b460e8bd0a36
sseong <73373959+sseong08@users.noreply.github.com>
2022-10-15 13:03:57 +0900

    Add files via upload

--- sleep/suv.js
+++ sleep/suv.js
@@ -5,12 +5,15 @@ function start(){
 	const b = Number(time.minute);
 	const c = time.ampm;
 	
-	const Fh = String(Math.abs(a+3)); //계산 식 다시 세워야함
-	const Sh = String(Math.abs(a+4)); // ㅅㄱㅋㅋ
-	const Th = String(Math.abs(a+6));
-	const Ffh = String(Math.abs(a+7));
-	const FFh = String(Math.abs(a+9));
-	const Ssh = String(Math.abs(a+10));
+	
+	let str1 = `am`
+	let str2 = `pm`
+	var Fh =  String(Math.abs(a+3)); //계산 식 다시 세워야함
+	var Sh =  String(Math.abs(a+4)); // ㅅㄱㅋㅋ
+	var Th =  String(Math.abs(a+6));
+	var Ffh = String(Math.abs(a+7));
+	var FFh = String(Math.abs(a+9));
+	var Ssh = String(Math.abs(a+10));
 	if(b< 30){
 		var Sm = String(Math.abs(b+30));
 		var Ffm = String(Math.abs(b+30));
@@ -20,9 +23,43 @@ function start(){
 		var Ffm = String(Math.abs(b-30));
 		var Ssf= String(Math.abs(b-30));
 	}
-
 	
-	
+	if(Fh >=13){
+		var Fh = '오전 ' + String(Math.abs(a+3-12));
+	}else{
+		var Fh = '오후 ' + String(Math.abs(a+3));
+	}
+	if(Sh>=13){
+		var Sh ='오전 ' + String(Math.abs(a+4-12));
+	}else{
+		var Sh = '오후 ' + String(Math.abs(a+4));
+	}
+	if(Th>=13){
+		var Th ='오전 ' + String(Math.abs(a+6-12));
+	}else{
+		var Th = '오후 ' + String(Math.abs(a+6));
+	}
+	if(Ffh>=13){
+		var Ffh  ='오전 ' + String(Math.abs(a+7-12));
+	}else{
+		var Ffh ='오후 ' + String(Math.abs(a+7));
+	}
+	if(FFh>=13){
+		var FFh= '오전 ' + String(Math.abs(a+9-12));
+	}else{
+		var FFh = '오후 ' +String(Math.abs(a+9));
+	}
+	if(Ssh>=13){
+		var Ssh= '오전 ' + String(Math.abs(a+10-12));
+	}else{
+		var Ssh ='오후 ' + String(Math.abs(a+10));
+	}
+	//	var Sh = String(Math.abs(a+4-12));
+	//	var Th = String(Math.abs(a+6-12));
+	//	var Ffh = String(Math.abs(a+7-12));
+	//	var FFh= String(Math.abs(a+9-12));
+	//	var Ssh= String(Math.abs(a+10-12));
+	//}
 	if (b==5){
 		document.write("<div class='main_section' styel='font-weight: 250;'>");
 		document.write("<h1 class='hour1' style=' margin-top: 10%; color : #f5d042;font-weight: 300; text-align:center;'> 이 시간에 자면 좋겠네요...</h1>")
@@ -45,3 +82,4 @@ function start(){
 		document.write("</div>")
 	}
 }
+console.log(Fhtime)
